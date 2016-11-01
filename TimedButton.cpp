37b6aa7f37fd0ca75af6e77c8669fb10b4fa3bf7
Evan Wills <evan.wills@acu.edu.au>
2016-11-01 19:52:18 +1100

    commented out most of the code to test if the basic class (DumbButton) works.

--- TimedButton.cpp
+++ TimedButton.cpp
@@ -56,36 +56,36 @@ void DumbButton::makePinModePullup() {
  *		 65 seconds. (Otherwise, You'll probabaly have to think of
  *		 another way of using this class.)
  */
-unsigned int TimedButton::pressed() {
-//	bool pressed = readButtion();
-
-	if( readButtion() == true ) {
-//	if( pressed == true ) {
-		if (start == 0 ) {
-			start = millis();
-		}
-		// update how long the button has been pressed for
-		pressDuration = millis() - start;
-		inUse = true;
-
-		// let the caller know that the button is being pressed
-		// but that we don't know for how long yet.
-		return -1;
-	} else {
-		// button is no longer being pressed.
-		// we keep the press duration for one call so
-		// that buttonPressedFor() can be used effectively
-		if( inUse == false ) {
-			// button was being pressed two calls ago
-			// reset for next press
-			start = 0;
-			pressDuration = 0;
-		}
-		// button is no longer being pressed
-		inUse = false;
-		return (unsigned int) pressDuration;
-	}
-}
+// unsigned int TimedButton::pressed() {
+// //	bool pressed = readButtion();
+//
+// 	if( readButtion() == true ) {
+// //	if( pressed == true ) {
+// 		if (start == 0 ) {
+// 			start = millis();
+// 		}
+// 		// update how long the button has been pressed for
+// 		pressDuration = millis() - start;
+// 		inUse = true;
+//
+// 		// let the caller know that the button is being pressed
+// 		// but that we don't know for how long yet.
+// 		return -1;
+// 	} else {
+// 		// button is no longer being pressed.
+// 		// we keep the press duration for one call so
+// 		// that buttonPressedFor() can be used effectively
+// 		if( inUse == false ) {
+// 			// button was being pressed two calls ago
+// 			// reset for next press
+// 			start = 0;
+// 			pressDuration = 0;
+// 		}
+// 		// button is no longer being pressed
+// 		inUse = false;
+// 		return (unsigned int) pressDuration;
+// 	}
+// }
 
 
 //  END:  TimedButton class
@@ -93,70 +93,70 @@ unsigned int TimedButton::pressed() {
 // START: MultiPressButton class
 
 
-MultiPressButton::MultiPressButton( byte pin , int maxNoPress ) {
-	btnPin = pin;
-	pinMode(btnPin, INPUT);
-	noPress = maxNoPress;
-}
-
-unsigned int MultiPressButton::pressed() {
-	if( readButtion() == true ) {
-		if ( inUse == false ) {
-			// the button has just been pressed
-			if ( counting == false ) {
-				// the button has just been pressed for the first
-				// time since it was reset. Lets remember that we're
-				// now tracking how many presses
-				counting = true;
-			}
-
-			// because this may be called many times during a single
-			// press we only want to increment once per presss so
-			// this is done within this IF statement
-
-			// add another press to the record
-			presses = presses + 1;
-			inUse = true;
-		}
-
-		// let the caller know that the button is being pressed
-		// but that we don't know what the total number of presses
-		// is yet.
-		return -1;
-	} else {
-		int output = 0;
-		if ( counting == true ) {
-			// we know we're counting now we'll check how long it was
-			// since the button was released
-			if ( inUse == true ) {
-				// this is the first time we've noticed the button
-				// has been released let's record that time
-				notPressed = millis();
-			}
-
-			// set how many milliseconds ago was the button released
-			int duration = millis() - notPressed;
-			if( duration > maxNoPress ) {
-				// the user has stopped pressing the button for
-				// the moment
-
-				// send the number of presses to the caller
-				output = presses;
-				// stop counting
-				counting = false;
-				// reset presses
-				presses = 0;
-				notPressed = 0;
-			} else {
-				// let the caller know we're still counting presses
-				output = -1;
-			}
-		}
-		// button is no longer being pressed
-		inUse = false;
-		return output;
-	}
-}
+// MultiPressButton::MultiPressButton( byte pin , int maxNoPress ) {
+// 	btnPin = pin;
+// 	pinMode(btnPin, INPUT);
+// 	noPress = maxNoPress;
+// }
+//
+// unsigned int MultiPressButton::pressed() {
+// 	if( readButtion() == true ) {
+// 		if ( inUse == false ) {
+// 			// the button has just been pressed
+// 			if ( counting == false ) {
+// 				// the button has just been pressed for the first
+// 				// time since it was reset. Lets remember that we're
+// 				// now tracking how many presses
+// 				counting = true;
+// 			}
+//
+// 			// because this may be called many times during a single
+// 			// press we only want to increment once per presss so
+// 			// this is done within this IF statement
+//
+// 			// add another press to the record
+// 			presses = presses + 1;
+// 			inUse = true;
+// 		}
+//
+// 		// let the caller know that the button is being pressed
+// 		// but that we don't know what the total number of presses
+// 		// is yet.
+// 		return -1;
+// 	} else {
+// 		int output = 0;
+// 		if ( counting == true ) {
+// 			// we know we're counting now we'll check how long it was
+// 			// since the button was released
+// 			if ( inUse == true ) {
+// 				// this is the first time we've noticed the button
+// 				// has been released let's record that time
+// 				notPressed = millis();
+// 			}
+//
+// 			// set how many milliseconds ago was the button released
+// 			int duration = millis() - notPressed;
+// 			if( duration > maxNoPress ) {
+// 				// the user has stopped pressing the button for
+// 				// the moment
+//
+// 				// send the number of presses to the caller
+// 				output = presses;
+// 				// stop counting
+// 				counting = false;
+// 				// reset presses
+// 				presses = 0;
+// 				notPressed = 0;
+// 			} else {
+// 				// let the caller know we're still counting presses
+// 				output = -1;
+// 			}
+// 		}
+// 		// button is no longer being pressed
+// 		inUse = false;
+// 		return output;
+// 	}
+// }
 
 
 //  END:  MultiPressButton class
@@ -164,103 +164,103 @@ unsigned int MultiPressButton::pressed() {
 // START: ToggleButton class
 
 
-ToggleButton::ToggleButton( byte pin , byte limit ) {
-	btnPin = pin;
-	pinMode(btnPin, INPUT);
-	max = limit;
-}
+// ToggleButton::ToggleButton( byte pin , byte limit ) {
+// 	btnPin = pin;
+// 	pinMode(btnPin, INPUT);
+// 	max = limit;
+// }
+//
+// unsigned int ToggleButton::pressed() {
+// 	if( readButtion() == true ) {
+// 		if ( inUse == false ) {
+// 			// because this may be called many times during a single
+// 			// press we only want to increment once per presss so
+// 			// this is done within this IF statement
+//
+// 			// add another press to the record
+// 			presses = presses + 1;
+//
+// 			// check how many times the button has been pressed since
+// 			// last reset and reset if necessary
+// 			if ( presses > max ) {
+// 				presses = 0;
+// 			}
+// 			inUse = true;
+// 		}
+// 	} else {
+// 		inUse = false;
+// 	}
+// 	return (unsigned int) presses;
+// }
 
-unsigned int ToggleButton::pressed() {
-	if( readButtion() == true ) {
-		if ( inUse == false ) {
-			// because this may be called many times during a single
-			// press we only want to increment once per presss so
-			// this is done within this IF statement
 
-			// add another press to the record
-			presses = presses + 1;
-
-			// check how many times the button has been pressed since
-			// last reset and reset if necessary
-			if ( presses > max ) {
-				presses = 0;
-			}
-			inUse = true;
-		}
-	} else {
-		inUse = false;
-	}
-	return (unsigned int) presses;
-}
-
-
-//  END:  MultiPressButton class
+//  END:  ToggleButton class
 // ========================================================
 // START: FixedTimeMultiPressButton class
 
 
-FixedTimeMultiPressButton::FixedTimeMultiPressButton( byte pin , int pressIntervalTime ) {
-	btnPin = pin;
-	pinMode(btnPin, INPUT);
-	pressInterval = pressIntervalTime;
-}
-
-unsigned int FixedTimeMultiPressButton::pressed() {
-	int output = 0;
-	// how long has it been since we started
-	int duration;
-	bool ended = true;
-
-	if( readButtion() == true ) {
-		output = -1;
-		if ( inUse == false ) {
-			// the button has just been pressed
-			if ( counting == false ) {
-				// the button has just been pressed for the first
-				// time since it was reset. Lets remember that we're
-				// now tracking how many presses
-				counting = true;
-
-				// start the timer
-				start = millis();
-			}
-
-			duration = millis() - start;
-			if ( duration < pressInterval ) {
-				// add another press to the record
-				presses = presses + 1;
-				inUse = true;
-			}
-		}
-
-		// let the caller know that the button is being pressed
-		// but that we don't know what the total number of presses
-		// is yet.
-		return output;
-	}
-
-	if ( counting == true ) {
-		duration = millis() - start;
-		if( duration > pressInterval ) {
-			// the user has stopped pressing the button for
-			// the moment
-
-			// send the number of presses to the caller
-			output = presses;
-			// stop counting
-			counting = false;
-			// reset presses
-			presses = 0;
-		} else {
-			// let the caller know we're still counting presses
-			output = -1;
-		}
-		inUse = false;
-	}
-	// button is no longer being pressed
-
-	return output;
-}
+// FixedTimeMultiPressButton::FixedTimeMultiPressButton( byte pin , int pressIntervalTime ) {
+// 	btnPin = pin;
+// 	pinMode(btnPin, INPUT);
+// 	pressInterval = pressIntervalTime;
+// }
+//
+// unsigned int FixedTimeMultiPressButton::pressed() {
+// 	int output = 0;
+// 	// how long has it been since we started
+// 	int duration;
+// 	bool ended = true;
+//
+// 	if( readButtion() == true ) {
+// 		output = -1;
+// 		if ( inUse == false ) {
+// 			// the button has just been pressed
+// 			if ( counting == false ) {
+// 				// the button has just been pressed for the first
+// 				// time since it was reset. Lets remember that we're
+// 				// now tracking how many presses
+// 				counting = true;
+//
+// 				// start the timer
+// 				start = millis();
+// 			}
+//
+// 			duration = millis() - start;
+// 			if ( duration < pressInterval ) {
+// 				// add another press to the record
+// 				presses = presses + 1;
+// 				inUse = true;
+// 			}
+// 		}
+//
+// 		// let the caller know that the button is being pressed
+// 		// but that we don't know what the total number of presses
+// 		// is yet.
+// 		return output;
+// 	}
+//
+// 	if ( counting == true ) {
+// 		duration = millis() - start;
+// 		if( duration > pressInterval ) {
+// 			// the user has stopped pressing the button for
+// 			// the moment
+//
+// 			// send the number of presses to the caller
+// 			output = presses;
+// 			// stop counting
+// 			counting = false;
+// 			// reset presses
+// 			presses = 0;
+// 		} else {
+// 			// let the caller know we're still counting presses
+// 			output = -1;
+// 		}
+// 		inUse = false;
+// 	}
+// 	// button is no longer being pressed
+//
+// 	return output;
+// }
 
 
 //  END:  FixedTimeMultiPressButton class
@@ -268,69 +268,69 @@ unsigned int FixedTimeMultiPressButton::pressed() {
 // START: MultiModeButton class
 
 
-MultiModeButton::MultiModeButton( byte pin , int pressIntervalTime ) {
-	btnPin = pin;
-	pinMode(btnPin, INPUT);
-	noPress = maxNoPress;
-}
+// MultiModeButton::MultiModeButton( byte pin , int pressIntervalTime ) {
+// 	btnPin = pin;
+// 	pinMode(btnPin, INPUT);
+// 	noPress = maxNoPress;
+// }
 
 // unsigned int MultiModeButton::pressed() inherrited from TimedButton
 
-int MultiModeButton::multiPress() {
-	if( readButtion() == true ) {
-		if ( multiPressInUse == false ) {
-			// the button has just been pressed
-			if ( counting == false ) {
-				// the button has just been pressed for the first
-				// time since it was reset. Lets remember that we're
-				// now tracking how many presses
-				counting = true;
-			}
-			// because this may be called many times during a single
-			// press we only want to increment once per presss so
-			// this is done within an IF statement
-
-			// add another press to the record
-			presses = presses + 1;
-			multiPressInUse = true;
-		}
-
-		// let the caller know that the button is being pressed
-		// but that we don't know what the total number of presses
-		// is yet.
-		return -1;
-	} else {
-		int output = 0;
-		if ( counting == true ) {
-			// we know we're counting now we'll check how long it was
-			// since the button was released
-			if ( multiPressInUse == true ) {
-				// this is the first time we've noticed the button
-				// has been released let's record that time
-				notPressed = millis();
-			}
-
-			// set how many milliseconds ago was the button released
-			int duration = millis() - notPressed;
-			if( duration > maxNoPress ) {
-				// the user has stopped pressing the button for
-				// the moment
-
-				// send the number of presses to the caller
-				output = presses;
-				// stop counting
-				counting = false;
-				// reset presses
-				presses = 0;
-				// reset the not pressed value
-				notPressed = 0;
-			} else {
-				// let the caller know we're still counting presses
-				output = -1;
-			}
-		}
-		// button is no longer being pressed
-		multiPressInUse = false;
-		return output;
-	}
-}
+// int MultiModeButton::multiPress() {
+// 	if( readButtion() == true ) {
+// 		if ( multiPressInUse == false ) {
+// 			// the button has just been pressed
+// 			if ( counting == false ) {
+// 				// the button has just been pressed for the first
+// 				// time since it was reset. Lets remember that we're
+// 				// now tracking how many presses
+// 				counting = true;
+// 			}
+// 			// because this may be called many times during a single
+// 			// press we only want to increment once per presss so
+// 			// this is done within an IF statement
+//
+// 			// add another press to the record
+// 			presses = presses + 1;
+// 			multiPressInUse = true;
+// 		}
+//
+// 		// let the caller know that the button is being pressed
+// 		// but that we don't know what the total number of presses
+// 		// is yet.
+// 		return -1;
+// 	} else {
+// 		int output = 0;
+// 		if ( counting == true ) {
+// 			// we know we're counting now we'll check how long it was
+// 			// since the button was released
+// 			if ( multiPressInUse == true ) {
+// 				// this is the first time we've noticed the button
+// 				// has been released let's record that time
+// 				notPressed = millis();
+// 			}
+//
+// 			// set how many milliseconds ago was the button released
+// 			int duration = millis() - notPressed;
+// 			if( duration > maxNoPress ) {
+// 				// the user has stopped pressing the button for
+// 				// the moment
+//
+// 				// send the number of presses to the caller
+// 				output = presses;
+// 				// stop counting
+// 				counting = false;
+// 				// reset presses
+// 				presses = 0;
+// 				// reset the not pressed value
+// 				notPressed = 0;
+// 			} else {
+// 				// let the caller know we're still counting presses
+// 				output = -1;
+// 			}
+// 		}
+// 		// button is no longer being pressed
+// 		multiPressInUse = false;
+// 		return output;
+// 	}
+// }
--- TimedButton.h
+++ TimedButton.h
@@ -21,23 +21,23 @@
 
 // ========================================================
 // START: ITimedButton interface
-
-class ITimedButton {
-
-	public:
-		/**
-		 * If button is pressed, starts counting the number of
-		 * milliseconds since start and returns true. If not,
-		 * resets the timer and returns false.
-		 */
-		virtual bool readButton();
-
-		/**
-		 * reads the button's state then returns the number of
-		 * milliseconds the buttons was pressed for.
-		 */
-		virtual unsigned int pressed();
-};
+//
+// class ITimedButton {
+//
+// 	public:
+// 		/**
+// 		 * If button is pressed, starts counting the number of
+// 		 * milliseconds since start and returns true. If not,
+// 		 * resets the timer and returns false.
+// 		 */
+// 		virtual bool readButton();
+//
+// 		/**
+// 		 * reads the button's state then returns the number of
+// 		 * milliseconds the buttons was pressed for.
+// 		 */
+// 		virtual unsigned int pressed();
+// };
 
 
 //  END:  ITimedButton interface
@@ -45,7 +45,7 @@ class ITimedButton {
 // START: (basic) DumbButton class
 
 
-class DumbButton : public ITimedButton {
+class DumbButton { // : public ITimedButton {
 
 	public:
 		DumbButton( byte pin );
@@ -77,27 +77,27 @@ class DumbButton : public ITimedButton {
 // START: TimedButton class
 
 
-class TimedButton : public DumbButton {
-
-	public:
-
-//		TimedButton(byte pin); // inherrited from DumbButton
-		/**
-		 * reads the button's state then returns the number of
-		 * milliseconds the buttons was pressed for.
-		 */
-		unsigned int pressed();
-
-	private:
-
-		/**
-		 * start the time in milliseconds (from when the Arduino was
-		 * turned on) the button was pressed.
-		 */
-		unsigned long start = 0;
-		unsigned long pressDuration = 0;
-		bool inUse = false;
-};
+// class TimedButton : public DumbButton {
+//
+// 	public:
+//
+// //		TimedButton(byte pin); // inherrited from DumbButton
+// 		/**
+// 		 * reads the button's state then returns the number of
+// 		 * milliseconds the buttons was pressed for.
+// 		 */
+// 		unsigned int pressed();
+//
+// 	private:
+//
+// 		/**
+// 		 * start the time in milliseconds (from when the Arduino was
+// 		 * turned on) the button was pressed.
+// 		 */
+// 		unsigned long start = 0;
+// 		unsigned long pressDuration = 0;
+// 		bool inUse = false;
+// };
 
 
 //  END:  TimedButton class
@@ -105,32 +105,32 @@ class TimedButton : public DumbButton {
 // START: MultiPressButton class
 
 
-class MultiPressButton : public DumbButton {
-
-	public:
-		MultiPressButton( byte pin , int maxNoPressInterval = 500 );
-		/**
-		 * reads the button's state then returns the number of
-		 * times the buttons was pressed.
-		 */
-		unsigned int pressed();
-
-	private:
-
-		/**
-		 * start the time in milliseconds (from when the Arduino was
-		 * turned on) the button was pressed.
-		 */
-		unsigned long notPressed = 0;
-		bool inUse = false;
-		bool counting = false;
-		byte presses = 0;
-		/**
-		 * the maximum number of milliseconds after the button was
-		 * released that indicates no more pressing has finished.
-		 */
-		int maxNoPress = 500;
-};
+// class MultiPressButton : public DumbButton {
+//
+// 	public:
+// 		MultiPressButton( byte pin , int maxNoPressInterval = 500 );
+// 		/**
+// 		 * reads the button's state then returns the number of
+// 		 * times the buttons was pressed.
+// 		 */
+// 		unsigned int pressed();
+//
+// 	private:
+//
+// 		/**
+// 		 * start the time in milliseconds (from when the Arduino was
+// 		 * turned on) the button was pressed.
+// 		 */
+// 		unsigned long notPressed = 0;
+// 		bool inUse = false;
+// 		bool counting = false;
+// 		byte presses = 0;
+// 		/**
+// 		 * the maximum number of milliseconds after the button was
+// 		 * released that indicates no more pressing has finished.
+// 		 */
+// 		int maxNoPress = 500;
+// };
 
 
 //  END:  MultiPressButton class
@@ -140,64 +140,64 @@ class MultiPressButton : public DumbButton {
 // ToggleButton can be used to manage which index of an Array should
 // be used at any given time
 
-class ToggleButton : public DumbButton {
+// class ToggleButton : public DumbButton {
+//
+// 	public:
+// 		ToggleButton( byte pin , byte limit = 1 );
+// 		/**
+// 		 * reads the button's state then returns the number of
+// 		 * times the buttons was pressed.
+// 		 */
+// 		unsigned int pressed();
+//
+// 	private:
+//
+// 		/**
+// 		 * start the time in milliseconds (from when the Arduino was
+// 		 * turned on) the button was pressed.
+// 		 */
+// 		bool inUse = false;
+// 		byte presses = 0;
+// 		/**
+// 		 * the maximum number of milliseconds after the button was
+// 		 * released that indicates no more pressing has finished.
+// 		 */
+// 		byte max = 1;
+// };
 
-	public:
-		ToggleButton( byte pin , byte limit = 1 );
-		/**
-		 * reads the button's state then returns the number of
-		 * times the buttons was pressed.
-		 */
-		unsigned int pressed();
 
-	private:
-
-		/**
-		 * start the time in milliseconds (from when the Arduino was
-		 * turned on) the button was pressed.
-		 */
-		bool inUse = false;
-		byte presses = 0;
-		/**
-		 * the maximum number of milliseconds after the button was
-		 * released that indicates no more pressing has finished.
-		 */
-		byte max = 1;
-};
-
-
-//  END:  toggleButton class
+//  END:  ToggleButton class
 // ========================================================
 // START: FixedTimeMultiPressButton class
 
 
-class FixedTimeMultiPressButton : public DumbButton {
-
-	public:
-		FixedTimeMultiPressButton( byte pin , int pressIntervalTime = 1000 );
-		/**
-		 * reads the button's state then returns the number of
-		 * times the buttons was pressed.
-		 */
-		unsigned int pressed();
-
-	private:
-
-		/**
-		 * start the time in milliseconds (from when the Arduino was
-		 * turned on) the button was pressed.
-		 */
-		unsigned long start = 0;
-		int pressInterval = 0;
-		bool inUse = false;
-		bool counting = false;
-		byte presses = 0;
-		/**
-		 * the maximum number of milliseconds after the button was
-		 * released that indicates no more pressing has finished.
-		 */
-		int TimeLimit = 1000;
-};
+// class FixedTimeMultiPressButton : public DumbButton {
+//
+// 	public:
+// 		FixedTimeMultiPressButton( byte pin , int pressIntervalTime = 1000 );
+// 		/**
+// 		 * reads the button's state then returns the number of
+// 		 * times the buttons was pressed.
+// 		 */
+// 		unsigned int pressed();
+//
+// 	private:
+//
+// 		/**
+// 		 * start the time in milliseconds (from when the Arduino was
+// 		 * turned on) the button was pressed.
+// 		 */
+// 		unsigned long start = 0;
+// 		int pressInterval = 0;
+// 		bool inUse = false;
+// 		bool counting = false;
+// 		byte presses = 0;
+// 		/**
+// 		 * the maximum number of milliseconds after the button was
+// 		 * released that indicates no more pressing has finished.
+// 		 */
+// 		int TimeLimit = 1000;
+// };
 
 
 //  END:  FixedTimeMultiPressButton class
@@ -207,45 +207,45 @@ class FixedTimeMultiPressButton : public DumbButton {
 
 // MultiModeButton alows for both long press and multi-press button
 // presses to be used on a single button
-class MultiModeButton : public TimedButton {
+// class MultiModeButton : public TimedButton {
 
-	public:
-		MultiModeButton( byte pin , int maxNoPressInterval = 500 );
-		/**
-		 * reads the button's state then returns how long the button
-		 * was pressed for (or -1 if the button is currently being
-		 * pressed)
-		 */
-		unsigned int pressed();
-
-		/**
-		 * reads the button's state then returns the number of
-		 * times the buttons was pressed or -1 if the button is
-		 * currently being pressed.
-		 *
-		 * functionality copied directly from MultiPressButton::pressed()
-		 * The only difference is the variable inUse is renamed to
-		 * multiPressInUse
-		 */
-		int multiPress();
-
-		// int MultiModeButton::pressed() inherrited from TimedButton
-
-	private:
-
-		/**
-		 * start the time in milliseconds (from when the Arduino was
-		 * turned on) the button was pressed.
-		 */
-		unsigned long notPressed = 0;
-		bool multiPressInUse = false;
-		bool counting = false;
-		byte presses = 0;
-		/**
-		 * the maximum number of milliseconds after the button was
-		 * released that indicates no more pressing has finished.
-		 */
-		int maxNoPress = 500;
-};
+// 	public:
+// 		MultiModeButton( byte pin , int maxNoPressInterval = 500 );
+// 		/**
+// 		 * reads the button's state then returns how long the button
+// 		 * was pressed for (or -1 if the button is currently being
+// 		 * pressed)
+// 		 */
+// 		unsigned int pressed();
+//
+// 		/**
+// 		 * reads the button's state then returns the number of
+// 		 * times the buttons was pressed or -1 if the button is
+// 		 * currently being pressed.
+// 		 *
+// 		 * functionality copied directly from MultiPressButton::pressed()
+// 		 * The only difference is the variable inUse is renamed to
+// 		 * multiPressInUse
+// 		 */
+// 		int multiPress();
+//
+// 		// int MultiModeButton::pressed() inherrited from TimedButton
+//
+// 	private:
+//
+// 		/**
+// 		 * start the time in milliseconds (from when the Arduino was
+// 		 * turned on) the button was pressed.
+// 		 */
+// 		unsigned long notPressed = 0;
+// 		bool multiPressInUse = false;
+// 		bool counting = false;
+// 		byte presses = 0;
+// 		/**
+// 		 * the maximum number of milliseconds after the button was
+// 		 * released that indicates no more pressing has finished.
+// 		 */
+// 		int maxNoPress = 500;
+// };
 
 #endif
